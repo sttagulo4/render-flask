712cfed38f356ffbab3921bcabcbf217021a9365
Shuvi Tagulo <sttagulo.student@asiancollege.edu.ph>
2025-04-07 09:56:20 +0800

    Add files via upload

---
+++ pythonProject/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        return render_template('index.html', name=name)
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
